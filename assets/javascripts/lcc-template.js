ed443b5dbcd608e3f40366614b725cd92e1d37dc
Travis CI <developer@leeds.gov.uk>
2017-03-08 17:58:59 +0000

    Publishing LCC sharepoint templates version 1.1.43

--- assets/javascripts/lcc-template.js
+++ assets/javascripts/lcc-template.js
@@ -6365,6 +6365,50 @@ var widgetsDatepicker = $.datepicker;
 	}
 	
     global.LCC = LCC;
+})(window, jQuery);(function (global, $) {
+    "use strict";
+    var LCC = global.LCC || {};
+	    LCC.Datepicker = LCC.Datepicker || {};
+
+    $(document).ready(function () {
+        $("input[data-type='datepicker-start']").datepicker({
+            defaultDate: null,
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            minDate: 0,
+            onSelect: function(selected) {
+                $("input[data-type='datepicker-end']").datepicker("option", "minDate", selected)
+            }
+        });
+        $("input[data-type='datepicker-end']").datepicker({
+            defaultDate: null,
+            dateFormat: "dd/mm/yy",
+            changeMonth: true,
+            minDate: 0,
+            onSelect: function (selected) {
+                $("input[data-type='datepicker-start']").datepicker("option", "maxDate", selected)
+            }
+        }); 
+        $("input[data-type='datepicker-start-nomin']").datepicker({
+            defaultDate: null,
+            dateFormat: "yy/mm/dd",
+            changeMonth: true,
+            onSelect: function(selected) {
+                $("input[data-type='datepicker-end-nomin']").datepicker("option", "minDate", selected)
+            }
+        });
+        $("input[data-type='datepicker-end-nomin']").datepicker({
+            defaultDate: null,
+            dateFormat: "yy/mm/dd",
+            changeMonth: true,
+            onSelect: function (selected) {
+                $("input[data-type='datepicker-start-nomin']").datepicker("option", "maxDate", selected)
+            }
+        });
+    });
+
+    global.LCC = LCC;
+
 })(window, jQuery);(function (global) {
     "use strict";
 
@@ -6527,6 +6571,7 @@ var widgetsDatepicker = $.datepicker;
 //(=) require sticky-ribbon
 //(=) require jquery-ui-datepicker
 //(=) require max-characters
+//(=) require datepicker
 //(=) require modules/back-to-top
 //(=) require modules/accordion
 //(=) require modules/carousel
